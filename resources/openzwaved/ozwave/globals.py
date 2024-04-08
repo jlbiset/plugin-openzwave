de3271d33c64b71c720e72d130bd5267560d7ce9
sarakha63 <sarakha_ludovic@yahoo.fr>
2024-04-08 12:10:25 +0200

    added fgr224

--- resources/openzwaved/ozwave/globals.py
+++ resources/openzwaved/ozwave/globals.py
@@ -18,6 +18,7 @@ REFRESH_MAPPING = {'2|3|32784': {'67|1|1' : {'onlyset' : '67|11|1','sleep' :1 , 
 					'271|7425|4096': {'38|1|0' : {'sleep' :5 , 'number' :4},'38|1|1' : {'sleep' :5 , 'number' :4, 'other':'38|1|0'},'38|1|2' : {'sleep' :5 , 'number' :4, 'other':'38|1|0'}},\
 					'271|7169|4096': {'38|1|0' : {'sleep' :3 , 'number' :3},'38|1|1' : {'sleep' :3 , 'number' :3, 'other':'38|1|0'},'38|1|2' : {'sleep' :3 , 'number' :3, 'other':'38|1|0'}},\
 					'271|2306|4096': {'51|1|0' : {'sleep' :5 , 'number' :3 , 'other' : '38|1|0'},'38|1|0' : {'sleep' :5 , 'number' :3 , 'other' : '51|1|0'}},\
+					'271|772|4096': {'38|1|0' : {'sleep' :9 , 'number' :6}, 'other':'38|1|0'}},\
 					'816|512|53253': {'38|1|0' : {'sleep' :3 , 'number' :3 , 'com' : 'sunricher dimmer'},'38|1|1' : {'sleep' :3 , 'number' :3, 'other':'38|1|0'},'38|1|2' : {'sleep' :3 , 'number' :3, 'other':'38|1|0'}},\
 					'816|512|53260': {'38|1|0' : {'sleep' :3 , 'number' :3 , 'com' : 'sunricher dimmer'},'38|1|1' : {'sleep' :3 , 'number' :3, 'other':'38|1|0'},'38|1|2' : {'sleep' :3 , 'number' :3, 'other':'38|1|0'}},\
 					'134|3|132': {'37|1|0' : {'sleep' :2 , 'number' :1 , 'com' : 'nanodimmer'},'37|2|0' : {'sleep' :2 , 'number' :1},'37|3|0' : {'sleep' :2 , 'number' :1}},\
